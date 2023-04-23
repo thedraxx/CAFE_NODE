2aeec2e850b8f0c6851e6c0c554038f928c58f85
thedraxx <thedraxxgames@gmail.com>
2023-04-23 18:36:31 -0300

    validar todos los campos necesarios

--- controllers/usuarios.js
+++ controllers/usuarios.js
@@ -2,6 +2,7 @@ const { response, request } = require('express');
 const bcryptjs = require('bcryptjs');
 const Usuario = require('../models/usuario');
 const { validarCampos } = require('../middlewares/validad-campos');
+const { validationResult } = require('express-validator');
 
 
 const usuariosGet = (req = request, res = response) => {
@@ -20,7 +21,11 @@ const usuariosGet = (req = request, res = response) => {
 
 const usuariosPost = async (req, res = response) => {
 
+    const errors = validationResult(req)
 
+    if (!errors.isEmpty()) {
+        return res.status(400).json(errors);
+    }
 
 
     const {
@@ -38,7 +43,6 @@ const usuariosPost = async (req, res = response) => {
     });
 
     // verificar que el correo existe
-
     const existeEmail = await Usuario.findOne({
         correo
     });
