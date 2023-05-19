e40d2a261fb482f02df64c9278f3a67718fbc4d8
thedraxx <thedraxxgames@gmail.com>
2023-05-19 14:53:13 -0300

    validar la extension

--- controllers/uploads.js
+++ controllers/uploads.js
@@ -1,7 +1,28 @@
+const path = require('path');
+
+
+
+
 const cargarArchivo = async (req, res = response) => {
-    res.json({
-        msg: 'Cargar archivo'
-    })
+
+    if (!req.files || Object.keys(req.files).length === 0 || !req.files.archivo) {
+        res.status(400).json({
+            msg: 'No files were uploaded.'
+        });
+        return;
+    }
+
+    const { archivo } = req.files;
+
+    const uploadPath = path.join(__dirname, '../uploads/', archivo.name);
+
+    archivo.mv(uploadPath, (err) => {
+        if (err) {
+            return res.status(500).json({ err });
+        }
+
+        res.json({ msg: 'File uploaded to ' + uploadPath });
+    });
 }
 
 
--- models/server.js
+++ models/server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const cors = require('cors');
 const { dbConnection } = require('../database/config');
+const fileUpload = require('express-fileupload');
 
 class Server {
 
@@ -41,6 +42,14 @@ class Server {
 
         // Directorio Público
         this.app.use(express.static('public'));
+
+        // FileUpload - Carga de archivos
+        this.app.use(fileUpload({
+            useTempFiles: true,
+            tempFileDir: '/tmp/',
+            createParentPath: true
+        }));
+
 
     }
 
